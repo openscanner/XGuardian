41162d0b35319d0e08ecbab8f622ce018306a3b1
wuyadong <wyd.npu@gmail.com>
2015-06-29 11:46:50 +0800

    Initial Commit

---
+++ XGuardian/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  XGuardian
+//
+//  Created by  吴亚冬 on 15/6/29.
+//  Copyright (c) 2015年 杭州网蛙科技. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
